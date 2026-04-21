b37291f65d41ecccc56e091be8b87b4f89f99405
Ossire <keeerosky@gmail.com>
2026-04-21 23:08:06 +0100

    added dynamic cors for local env or live link

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors({
-    origin: 'https://e-commerce-angular-frontend.onrender.com',
+    origin: [
+      'https://e-commerce-angular-frontend.onrender.com',
+      'http://localhost:4200',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: 'Content-Type, Accept, Authorization',
